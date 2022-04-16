aab0f0e0b94b44b6a6dc88d677fcd22e730ccfaf
Luciano Neto <85460179+LucianoSabino@users.noreply.github.com>
2022-04-16 15:39:14 -0300

    Coleções e Interfaces Gráficas - Estilos CSS
    Fazendo a interface gráficas.
    Projeto finalizado.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Projeto-Poo
- Criação de um projeto oriientado a objeto.
-- Coleções e Interfaces Gráficas.
-    - Fazendo o arquivo modificador percorre o array ordena e capitaliza.
-Criação de um laço for para implanta a interface gráfica no html. 
+Criação de um projeto oriientado a objeto.
+- Coleções e Interfaces Gráficas - Estilos CSS.
+    - Fazendo a interface gráficas.
+### Finalizado o projeto. :)
 
--- script.js
+++ script.js
@@ -6,11 +6,12 @@ modificador.capitalizarNovo(ingredientes, 'nome')
 let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
 let cotainerIngredientes = document.querySelector('#container-ingredientes');
 
+
 for (let ingrediente of ingredientesOrdenados) {
     let tetoHTML = `
     <div class="ingrediente">
         <img src="img/${ingrediente.img}" alt="">
-        <p class="nome-ingrediente">${ingrediente.nome}</p>
+        <p class="nome-ingrediente" id="nav">${ingrediente.nome}</p>
     </div>
     `;
 
